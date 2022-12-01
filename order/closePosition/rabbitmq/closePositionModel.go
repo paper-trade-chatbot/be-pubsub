6145ca0ae7eff60a8f992a6fba631e3346d67261
Josh <JoshTsai@trevi.cc>
2022-12-01 11:09:17 +0800

    add: open price

--- order/closePosition/rabbitmq/closePositionModel.go
+++ order/closePosition/rabbitmq/closePositionModel.go
@@ -13,8 +13,10 @@ const (
 type ClosePositionModel struct {
 	ID           uint64          `valid:"required" json:"id"`
 	MemberID     uint64          `valid:"required" json:"memberID"`
+	PositionID   uint64          `valid:"required" json:"positionID"`
 	ExchangeCode string          `valid:"required" json:"exchangeCode"`
 	ProductCode  string          `valid:"required" json:"productCode"`
 	TradeType    TradeType       `valid:"required" json:"tradeType"`
-	CloseAmount  decimal.Decimal `valid:"required" json:"amount"` // 為正數，要平多少數量的倉
+	OpenPrice    decimal.Decimal `valid:"required" json:"openPrice"` // 開倉價
+	CloseAmount  decimal.Decimal `valid:"required" json:"amount"`    // 為正數，要平多少數量的倉
 }
